bb523aa6a21241f07942899c60df7dff81215ac0
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-01-19 12:14:35 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+let arr = []
+const returnFirstTwoDrivers = function (arr) {
+    return arr.slice(0, 2)
+}
+
+console.log(returnFirstTwoDrivers(['YSN', 'AHMED', 'QASSEM', 'ABDO']));
+
+let arrOne = []
+const returnLastTwoDrivers = function (arrOne) {
+    return arrOne.slice(arrOne.length - 2)
+}
+
+console.log(returnLastTwoDrivers(['YSN', 'AHMED', 'QASSEM', 'ABDO']));
+
+let selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+function createFareMultiplier(num) {
+    return function (num2) {
+        return num * num2
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+fareDoubler(10);
+
+const fareTripler = createFareMultiplier(3);
+fareTripler(12)
+let drivers  = []
+function selectDifferentDrivers(drivers , fun){
+      return fun(drivers)
+} 
+
+selectDifferentDrivers(['YSN', 'AHMED', 'QASSEM', 'ABDO'] , returnFirstTwoDrivers())
